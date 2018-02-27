1639a3ddbd30bea3f92e4bea16cce8d85529e976
Andreas Petutschnig <andreas@petutschnig.de>
2018-02-27 14:43:44 +0100

    fix Rcpp imports

--- R/twitterbots-package.r
+++ R/twitterbots-package.r
@@ -4,6 +4,7 @@
 #' @docType package
 #' @author Andreas Petutschnig
 #' @importFrom magrittr %>%
+#' @importFrom Rcpp evalCpp
 #' @importFrom tibble tibble
 #' @importFrom utils adist setTxtProgressBar txtProgressBar
 #' @useDynLib twitterbots, .registration = TRUE
